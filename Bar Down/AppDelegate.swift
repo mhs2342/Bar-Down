fe59f7259ed8ec6255b475a9048e62dea87a63e3
Matthew Sanford <mhs2342@vt.edu>
2019-12-25 11:18:58 -0500

    Initial Commit

---
+++ Bar Down/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Bar Down
+//
+//  Created by Matthew Sanford on 12/25/19.
+//  Copyright © 2019 sanch. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
